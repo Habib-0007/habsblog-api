ee1b55e2d0441e6a2c8ce0cdc377157332338e63
Habib-0007 <adebayohabib7@gmail.com>
2025-02-02 10:26:37 +0100

    adjusted the MongoDB URI

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = () => {
   mongoose
-    .connect(process.env.MONGODB_URI)
+    .connect(process.env.MONGODB_URI as string)
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('MongoDB connection error:', err as Error));
 };
