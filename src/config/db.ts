f380ac8a17393b57389a3bb7c27dd07ee7be3baf
Habib-0007 <adebayohabib7@gmail.com>
2025-02-02 03:02:13 +0100

    adjusted the cors

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = () => {
   mongoose
-    .connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/blog-app')
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('MongoDB connection error:', err as Error));
 };
