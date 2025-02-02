c7f8fcdd7a0813592aaee4df3c32b3426686aac4
Habib-0007 <adebayohabib7@gmail.com>
2025-02-02 10:39:59 +0100

    solve the errpr issues

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,49 +2,47 @@ import mongoose, { type Document, Schema } from 'mongoose';
 import bcrypt from 'bcryptjs';
 
 export interface IUser extends Document {
-  _id: string;
-  username: string;
-  email: string;
-  password: string;
-  profilePicture: string;
-  comparePassword: (candidatePassword: string) => Promise<boolean>;
-  resetPasswordToken?: string | undefined;
-  resetPasswordExpires?: Date | number;
+	username: string;
+	email: string;
+	password: string;
+	profilePicture: string;
+	comparePassword: (candidatePassword: string) => Promise<boolean>;
+	resetPasswordToken?: string | undefined;
+	resetPasswordExpires?: Date | number;
 }
 
 const UserSchema: Schema = new Schema(
-  {
-    _id: { type: String },
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    profilePicture: { type: String, default: '' },
-    resetPasswordToken: {
-      type: String || undefined,
-    },
-    resetPasswordExpires: {
-      type: Date || Number,
-    },
-  },
-  { timestamps: true },
+	{
+		username: { type: String, required: true, unique: true },
+		email: { type: String, required: true, unique: true },
+		password: { type: String, required: true },
+		profilePicture: { type: String, default: '' },
+		resetPasswordToken: {
+			type: String || undefined,
+		},
+		resetPasswordExpires: {
+			type: Date || Number,
+		},
+	},
+	{ timestamps: true },
 );
 
 UserSchema.pre<IUser>('save', async function (next) {
-  if (!this.isModified('password')) return next();
+	if (!this.isModified('password')) return next();
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error: any) {
-    next(error);
-  }
+	try {
+		const salt = await bcrypt.genSalt(10);
+		this.password = await bcrypt.hash(this.password, salt);
+		next();
+	} catch (error: any) {
+		next(error);
+	}
 });
 
 UserSchema.methods.comparePassword = async function (
-  candidatePassword: string,
+	candidatePassword: string,
 ): Promise<boolean> {
-  return bcrypt.compare(candidatePassword, this.password);
+	return bcrypt.compare(candidatePassword, this.password);
 };
 
 export default mongoose.model<IUser>('User', UserSchema);
